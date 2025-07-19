77b40e54b97190f0e7097b0348a0b0681901a33c
luckkalpna <luckkalpna@gmail.com>
2025-07-19 15:49:39 +0530

    Connect DataBase with mysqli & PDO

---
+++ website/sql-db-conn.php
@@ -0,0 +1,40 @@
+<?php
+// Connect db with mysqli
+
+// $host="localhost";
+// $username="root";
+// $password=NULL;
+// $dbname="college";
+
+
+// $conn = new mysqli($host, $username, $password, $dbname);
+// if($conn->connect_error){
+//   die("Connaction failed: ". $conn->connect_error).fetch_all();
+// }
+
+// echo "Connected successfully to the database.";
+// echo "<br/>";
+
+// $result = $conn->query("SHOW TABLES");
+// print_r($result);
+
+// Connect db with PDO (PHP Data Objects)
+
+$host = "localhost";
+$username = "root";
+$password = NULL;
+
+try {
+  $conn =  new PDO("mysql:host=$host;dbname=college", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully to the database using PDO.";
+} catch (PDOException $err) {
+  echo "Connection failed: " .$err->getMessage();
+}
+
+echo "<br/>";
+$result = $conn->query("SHOW TABLES");
+foreach ($result as$row) {
+  print_r($row);
+}
+?>
